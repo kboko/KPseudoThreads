ecc94a3e2457cafc730fe21296f8147aa1e64107
Kaloyan Stoilov <ksv@seh.de>
2022-10-24 13:15:24 +0200

    Adding Tests

---
+++ Tests/Test1_TimerThreads.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+"""
+MIT License
+
+Copyright (c) 2022 Kaloyan Stoilov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
+import sys
+sys.path.append('..')
+from mypseudothreads import *
+import traceback
+import random 
+import time
+from datetime import datetime
+"""
+	This Test starts a lot of timer threads and display the times when the thread should have been 
+	stated and the time it actually was fired.
+	- Threads are scheduled in random range between 1-5000 ms. /thread_min_time thread_max_time
+	- After each thread is fired predefined count times, it is no longer rescheduled
+	- Threads count/Number to be executed each thread is specified at the class creation
+	- DISPLAY=false is to disable printing on the console
+
+	User can check the performance of the threads code with many timer threads. Depending on the machine the delays may be different
+	
+"""
+DISPLAY=True
+class TimerThreadTest_1(MyPseudoThreads):
+	
+	def __init__(self, count_threads, max_count):
+		MyPseudoThreads.__init__(self, "SimpleStateMachineApp", LOG_DBG, LOG_CONSOLE)
+		self.max_delay = 0
+		self.max_count = max_count
+		self.count_threads = count_threads
+		self.thread_min_time = 100
+		self.thread_max_time = 5000
+		self.all_threads_executed = 0
+
+	def init_threads(self):
+		for a in range(self.count_threads):
+			execute_after_ms = random.randint(self.thread_min_time, self.thread_max_time)
+			thread = self.add_timer_thread("Timer_{}".format (a), execute_after_ms, self.timer_fire, None)
+			thread.count = 1
+
+		
+	def timer_fire(self, thread, arg):
+		now = time.time_ns()
+		self.all_threads_executed = self.all_threads_executed + 1
+		if DISPLAY: print (datetime.now(), thread.thread_name, "FIRE count=", thread.count, "Delayed = ", (now - thread.time)/1000000, "ms")
+		# record the max time
+		if (now - thread.time) > self.max_delay:
+			self.max_delay = now - thread.time
+
+		execute_after_ms = random.randint(self.thread_min_time, self.thread_max_time)
+		if thread.count < self.max_count:
+			if DISPLAY: print (datetime.now(), thread.thread_name, "Schedule", execute_after_ms)
+			new_thread = self.add_timer_thread(thread.thread_name, execute_after_ms, self.timer_fire, None)
+			new_thread.count = thread.count + 1
+
+
+def main():
+	# MAIN
+	something = TimerThreadTest_1( 5000, 10)
+	try:
+		something.Log(LOG_INFO, "Starting",)
+		something.init_threads()
+		something.threads_run();
+		print ("Max Delayed Thread= {} ms. All threads executed count {} should be {}".format(something.max_delay/1000000, something.all_threads_executed, something.max_count * something.count_threads))
+		something.Log(LOG_INFO, "All threads done. Stop")              
+	except KeyboardInterrupt:
+		traceback.print_exc()
+		exit (1)
+	except:
+		traceback.print_exc()
+	
+if __name__ == "__main__":
+	main()    
---
+++ Tests/Test2_ServerClient.py
@@ -0,0 +1,213 @@
+#!/usr/bin/python3
+"""
+MIT License
+
+Copyright (c) 2022 Kaloyan Stoilov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
+import sys
+sys.path.append('..')
+from mypseudothreads import *
+import traceback
+import random 
+import time
+import socket
+from datetime import datetime
+"""
+    starts a lot of timer threads and display the time when the thread should habe been 
+    stated and the time it actually was fired.
+    threads are scheduled in random range between 1-5000 ms.
+    After each thread is fired predefined count, it is no longer rescheduled
+"""
+DISPLAY=True
+PORT=34455
+DATA_PORTION = 2
+class Server(MyPseudoThreads):
+    
+    def __init__(self):
+        self.msg_size = DATA_PORTION
+        self.conn = None
+        self.addr = None
+        MyPseudoThreads.__init__(self, "Server", LOG_DBG, LOG_CONSOLE)
+        
+    def init_server(self):
+        for res in socket.getaddrinfo("127.0.0.1", PORT , socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
+            af, socktype, proto, canonname, sa = res
+            self.s = socket.socket(af, socktype, proto)
+            self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.s.bind(sa)
+            self.s.listen(1)
+            self.s.setblocking(0)
+            self.add_read_thread ("accept_client", self.s, self.accept_client, None)
+            
+    def accept_client(self, thread, arg):
+        con, addr = self.s.accept()
+        if self.conn and self.addr:
+            con.send("Second Connection to 13000 is not allowed\n>")
+            con.close()
+            self.add_read_thread ("accept_client", self.s, self.accept_client, None)
+            return
+        self.conn = con
+        self.addr = addr
+        self.counter_read_all = 0
+        self.counter_send_all = 0
+        print ('Command thread connected by {}'.format (self.addr))
+
+        
+        self.read_buffer = bytearray()
+        self.write_buffer = bytearray(DATA_PORTION)
+        for b in range(0, DATA_PORTION):
+            self.write_buffer[b] = b%256
+        self.write_index = 0
+
+        self.add_read_thread ("read_from_client", self.conn, self.read_from_client, None)
+        self.add_timer_thread("Print_statistic", 5000, self.timer_print_stat, None)
+        return True
+    
+    def timer_print_stat(self, thread, arg):
+        print ("So far: {} Reads, {} Writes, Rate".format(self.counter_read_all, self.counter_send_all))
+        self.add_timer_thread("Print_statistic", 5000, self.timer_print_stat, None)
+
+
+    def read_from_client(self, thread, arg):
+        try:
+            read_bytes = self.conn.recv(self.msg_size)
+        except:
+            read_bytes = None
+        # peer closed the connection or error
+        if read_bytes == None:
+            # cancel all threads
+            self.cancel_thread_by_sock(self.conn)
+            # close the socket
+            self.conn.close() 
+            self.conn=None 
+            # now start accepting again
+            self.add_read_thread ("accept_client", self.conn, self.accept_client, None)
+            return
+        self.counter_read_all = self.counter_read_all + len (read_bytes)
+        # add new bytes to our in buffer
+        self.read_buffer = self.read_buffer + read_bytes
+        # if we got whole message
+        if len(self.read_buffer) >= self.msg_size: 
+            in_data = self.read_buffer[:self.msg_size]
+            self.read_buffer = self.read_buffer[self.msg_size:]
+            #print ("Received {}, replying...".format(len(in_data)))
+            self.add_write_thread ("write_to_client", self.conn, self.write_to_client, None)
+        # more to read
+        self.add_read_thread ("read_from_client", self.conn, self.read_from_client, None)
+    
+    def write_to_client(self, thread, arg):
+        try:
+            sent = self.conn.send(self.write_buffer[self.write_index:])
+        except:
+            self.cancel_thread_by_sock(self.conn)
+            # close the socket
+            self.conn.close() 
+            self.conn=None 
+            exit(1)
+        self.counter_send_all = self.counter_send_all + sent
+        if sent != DATA_PORTION - self.write_index:
+            print ("Sent Only {}. Schedule more to send", sent)
+            self.write_index = self.write_index + sent
+            # schedule thread to send the reset
+            self.add_write_thread ("more_to_write_to_client", self.conn, self.write_to_client, None)
+        else:
+            self.sent_chunk = 0
+
+
+
+
+class Client(MyPseudoThreads):
+    
+    def __init__(self):
+        self.msg_size = DATA_PORTION
+        self.read_buffer = bytearray()
+        self.write_buffer = bytearray(DATA_PORTION)
+        for b in range(0, DATA_PORTION):
+            self.write_buffer[b] = b%256
+        self.write_index = 0
+
+        MyPseudoThreads.__init__(self, "Client", LOG_DBG, LOG_CONSOLE)
+        
+    def init_client(self):
+        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+        self.conn.connect(("127.0.0.1", PORT));
+        self.add_write_thread ("send_to_server", self.conn, self.send_to_server, None)
+        self.add_read_thread ("read_from_server", self.conn, self.read_from_server, None)   
+    
+    def read_from_server(self, thread, arg):
+        try:
+            read_bytes = self.conn.recv(self.msg_size)
+        except:
+            print ("Except")
+            read_bytes = None
+        # peer closed the connection or error
+        if read_bytes == None:
+            # cancel all threads
+            self.cancel_thread_by_sock(self.conn)
+            # close the socket
+            self.conn.close() 
+            self.conn=None 
+            # now start accepting again
+            self.add_read_thread ("accept_client", self.conn, self.accept_client, None)
+            return
+        # add new bytes to our in buffer
+        self.read_buffer = self.read_buffer + read_bytes
+        # if we got whole message
+        if len(self.read_buffer) >= self.msg_size: 
+            in_data = self.read_buffer[:self.msg_size]
+            self.read_buffer = self.read_buffer[self.msg_size:]
+            #print ("Received {}, replying...".format(len(in_data)))
+            self.add_write_thread ("send_to_server", self.conn, self.send_to_server, None)
+        # more to read
+        self.add_read_thread ("read_from_server", self.conn, self.read_from_server, None)
+    
+    def send_to_server(self, thread, arg):
+        try:
+            sent = self.conn.send(self.write_buffer[self.write_index:])
+        except:
+            self.cancel_thread_by_sock(self.conn)
+            # close the socket
+            self.conn.close() 
+            self.conn=None 
+            exit(1)
+        if sent != DATA_PORTION - self.write_index:
+            print ("Sent Only {}. Schedule more to send", sent)
+            self.write_index = self.write_index + sent
+            # schedule thread to send the reset
+            self.add_write_thread ("more_to_write_to_client", self.conn, self.send_to_server, None)
+        else:
+            #print ("Sent {}", sent)
+            self.sent_chunk = 0
+
+
+def main():
+    if len(sys.argv) > 1:
+        print ("Starting server")
+        # MAIN
+        something = Server()
+        something.init_server()
+        something.threads_run();
+    else:
+        print ("Starting Client")
+        something = Client()
+        something.init_client()
+        something.threads_run();
+if __name__ == "__main__":
+    main()    
